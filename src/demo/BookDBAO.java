8c69fb75794c419ff5a8e448d03a139dbe9e45ee
xinhan <xinhan_96@hotmail.com>
2015-10-29 17:56:10 +0800

    Initial version

---
+++ src/demo/BookDBAO.java
@@ -0,0 +1,27 @@
+package demo;
+
+/**
+ * Created by xinhan on 29/10/2015.
+ */
+public class BookDBAO {
+    public BookDetails getBookDetails(String bookId) {
+        // return dummy book for testing now
+        return createDummyBook(bookId);
+    }
+
+    // convenient method to create a dummy book detail
+    // this method is meant for testing
+    private BookDetails createDummyBook(String bookId) {
+        BookDetails details = new BookDetails();
+        details.setBookId(bookId);
+        details.setDescription("Web components for developers talks about how web components can transfor the way you develop application for the web");
+        details.setFirstName("Java");
+        details.setSurname("Expert");
+        details.setInventory(5);
+        details.setOnSale(false);
+        details.setPrice(5.9f);
+        details.setTitle("Web components for developers");
+        details.setYear(2015);
+        return details;
+    }
+}
